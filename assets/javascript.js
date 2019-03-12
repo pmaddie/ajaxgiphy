896877e545b14d1187e9a17d2aa3643e7bdcb694
Maddie P <madelineprusak@gmail.com>
2019-03-11 21:32:44 -0400

    added kitties button

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     // Event listener for all button elements
     // $("button").on("click", function() {
 
-    var topicsArray = ["frenchies", "sunsets", "reactions", "surprised", "babies", "silly", "beaches", "family", "funny", "Sun", "fall", "puppies", "cute"];
+    var topicsArray = ["frenchies", "kitties", "sunsets", "reactions", "surprised", "babies", "silly", "beaches", "family", "funny", "Sun", "fall", "puppies", "cute"];
 
 
 
